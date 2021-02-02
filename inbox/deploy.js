7e307ba8236ce71aedbc8ba74ca2cac0d45605be
Vladis <vladismarkin@gmail.com>
2021-02-02 15:01:19 +0200

    Done!

---
+++ inbox/deploy.js
@@ -0,0 +1,25 @@
+const HDWalletProvider = require("truffle-hdwallet-provider");
+const Web3 = require("web3");
+const { interface, bytecode } = require("./compile");
+
+const provider = new HDWalletProvider(
+  "girl pluck love flag unveil wave pool very expand wool reward vendor",
+  "https://rinkeby.infura.io/v3/97db390fb48c4bcd8568ed76db6e1111"
+);
+
+const web3 = new Web3(provider);
+// getAccounts() becouse the mnemonic can genarate many accounts.
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+  console.log("Attempting to deploy from account", accounts[0]);
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({
+      data: bytecode,
+      arguments: ["Hi there!"],
+    })
+    .send({ gas: "1000000", from: accounts[0] });
+
+  console.log("Contract deployed to", result.options.address);
+};
+
+deploy();
